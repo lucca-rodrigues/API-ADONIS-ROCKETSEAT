2f4ab9157fbb22b14e4443ce9d9fa906f961a866
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2020-11-12 17:57:33 -0300

    reset password

--- app/Controllers/Http/ResetPasswordController.js
+++ app/Controllers/Http/ResetPasswordController.js
@@ -16,32 +16,31 @@ class ResetPasswordController {
 
       await user.save()
 
-      await
-        Mail.send(['emails.reset_password'],
-        {email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}`
-      },
+      await Mail.send(
+        ['emails.reset_password'],
+        {email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}`},
         message => {
           message
           .to(user.email)
           .from('contato.luccarodrigues@gmail.com', 'Lucas | Rocketseat')
           .subject('Recuperação de senha')
-      })
-
+        }
+      )
       // return response.status(200).send({ message: 'Solicitação de senha realizada com sucesso!'})
-
     } catch (error) {
-      return response.status(error.status).send({ error: { message: 'Ops! Algo de errado não esta certo!'}})
+        return response.status(error.status).send({ error: { message: 'Ops! Algo de errado não esta certo!'}})
     }
   }
 
   async update ({ request, response }){
     try {
-      const email = request.input('email')
-      const user = await User.findByOrFail('email', email)
+      const { token, password } = request.all()
+      const user = await User.findByOrFail('token', token)
 
       const tokenExpired = moment()
-      .subtract('2', 'days') // Compara se a data do token é anterior a 2 dias
+      .subtract('2', 'days')
       .isAfter(user.token_created_at)
+      // Compara se a data do token é anterior a 2 dias
 
       if(tokenExpired){
         return response
